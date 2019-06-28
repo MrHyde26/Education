a200c262e0dd8a184fa05ff743d03f70c2dc512e
Artem Kravchenko <a.kravchenko@intetics.com>
2019-06-28 14:25:42 +0300

    --

--- Education/Class1.cs
+++ Education/Class1.cs
@@ -3,44 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Education
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 }
