2e6e614d519ad93878ee0bf751308490a22a6b44
Artem Kravchenko <a.kravchenko@intetics.com>
2019-07-13 12:31:10 +0300

    task1

---
+++ ClassWork1307/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork1307
+{
+   
+
+    public class Player
+    {
+        public Pong pong;
+        public Ping ping;
+
+        public Player(Pong pong, Ping ping)
+        { 
+        this.ping = new Ping();
+        this.pong = new Pong();
+
+        this.ping.EventPing +=OnPingInvoked;
+            this.pong.EventPong +=OnPongInvoked;
+
+            this.ping.DoPing();
+        }
+
+        public void OnPingInvoked(object sender, EventArgs e)
+        {
+            pong.DoPong();
+        }
+
+        public void OnPongInvoked(object sender, EventArgs e)
+        {
+            ping.DoPing();
+        }
+    }
+
+   
+}
--- ClassWork1307/Program.cs
+++ ClassWork1307/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Education
+namespace ClassWork1307
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
+            new Player(new Pong(), new Ping());
         }
+ 
     }
 }
